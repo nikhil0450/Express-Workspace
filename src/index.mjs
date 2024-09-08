45d41394847e38d33ef72090cb4e688d9c5a95d0
Nikhil Kulkarni <nikhilkulkarni2208@gmail.com>
2024-09-08 17:40:47 +0530

    query params

--- src/index.mjs
+++ src/index.mjs
@@ -5,28 +5,44 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
-    { id: 1, userName: "Jack123", fullName: "Jackson Benny"},
-    { id: 2, userName: "Henry456", fullName: "Henry Pie"},
-    { id: 3, userName: "Adam789", fullName: "Adam Den"}
-]
+  { id: 1, userName: "Jack123", fullName: "Jackson Benny" },
+  { id: 2, userName: "Henry456", fullName: "Henry Pie" },
+  { id: 3, userName: "Adam789", fullName: "Adam Den" },
+];
 
-app.get("/", (request, response)=>{
-    response.status(201).send({ msg: "Hello world"});
-})
+app.get("/", (request, response) => {
+  response.status(201).send({ msg: "Hello world" });
+});
 
-app.get("/users", (request, response)=>{
-    response.send(mockUsers)
-})
+app.get("/users", (request, response) => {
+  response.send(mockUsers);
+});
 
 // Request Params
-app.get("/api/users/:id", (request, response)=>{
-    const parsedId = parseInt(request.params.id);
-    if(isNaN(parsedId)) return response.status(400).send({msg: "Bad request. Invalid Id"});
-    const findUser = mockUsers.find((user)=> user.id === parsedId)
-    if(!findUser) return response.status(404).send({msg: "Error, User not found!"})
-    return response.send(findUser)
-})
+app.get("/api/users/:id", (request, response) => {
+  const parsedId = parseInt(request.params.id);
+  if (isNaN(parsedId))
+    return response.status(400).send({ msg: "Bad request. Invalid Id" });
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser)
+    return response.status(404).send({ msg: "Error, User not found!" });
+  return response.send(findUser);
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Running on Port ${PORT}`);
-})
+// Query Params
+app.get("/api/users", (request, response) => {
+  const {
+    query: { filter, value },
+  } = request;
+  if (filter && value)
+    return response.send(
+      mockUsers.filter((user) => {
+       return user[filter].includes(value);
+      })
+    );
+  return response.send(mockUsers);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
+});
