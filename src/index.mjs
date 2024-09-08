21c8b5b2d56699550f77e1b901aa0022f2e9c8bd
Nikhil Kulkarni <nikhilkulkarni2208@gmail.com>
2024-09-08 22:10:11 +0530

    delete

--- src/index.mjs
+++ src/index.mjs
@@ -80,6 +80,17 @@ app.patch("/api/users/:id", (request, response)=>{
     return response.sendStatus(200)
 })
 
+// Delete
+app.delete("/api/users/:id", (request, response)=>{
+    const { params: { id } } = request;
+    const parsedId = parseInt(id);
+    if(isNaN(parsedId)) return response.sendStatus(400);
+    const findUserIndex = mockUsers.findIndex(user => user.id === parsedId);
+    if(findUserIndex === -1) return response.sendStatus(404);
+    mockUsers.splice(findUserIndex, 1)
+    return response.sendStatus(200)
+})
+
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
 });
