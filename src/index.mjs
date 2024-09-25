db81d1ca939a7d1d7384c832f63fb5b21449f357
Nikhil Kulkarni <nikhilkulkarni2208@gmail.com>
2024-09-25 09:15:23 +0530

    loggingMiddleware

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,13 @@
 import express from "express";
 import usersRouter from "./routes/users.mjs";
 import handleFindUserIndex from "./utils/middlewares.mjs"
+import loggingMiddleware from "./utils/loggingMiddleware";
 
 const app = express();
 app.use(express.json());
 app.use(usersRouter)
 
 const PORT = process.env.PORT || 3000;
-
-const loggingMiddleware = (request, response, next) => {
-  console.log(`${request.method} - ${request.url}`);
-  next();
-};
 
 app.use(loggingMiddleware);
 
