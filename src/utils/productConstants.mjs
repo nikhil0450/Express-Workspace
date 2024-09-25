6e0659c4d777e4dc52fc86f388ad2ee9ecd6cb6a
Nikhil Kulkarni <nikhilkulkarni2208@gmail.com>
2024-09-25 09:55:22 +0530

    constants

---
+++ src/utils/productConstants.mjs
@@ -0,0 +1,13 @@
+export const mockProducts = [
+    { id: 1 , productName: "Milk", price : 35 },
+    { id: 2 , productName: "Bread", price : 25 },
+    { id: 3 , productName: "Eggs", price : 60 },
+    { id: 4 , productName: "Butter", price : 85 },
+    { id: 5 , productName: "Cheese", price : 120 },
+    { id: 6 , productName: "Yogurt", price : 40 },
+    { id: 7 , productName: "Paneer", price : 90 },
+    { id: 8 , productName: "Curd", price : 30 },
+    { id: 9 , productName: "Honey", price : 150 },
+    { id: 10 , productName: "Jam", price : 70 }
+  ]
+  
